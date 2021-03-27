b8c8e55873a2fe36c9c4c1abd1560b6799d68d18
Alex Ballas <alex@ballas.org>
2021-03-27 21:19:53 +0200

    Added the rancid script

---
+++ rancid/go.mod
@@ -0,0 +1,8 @@
+module rancid
+
+go 1.16
+
+require (
+	github.com/gen2brain/dlgs v0.0.0-20210324163825-f2c7498414a6
+	github.com/gopherjs/gopherjs v0.0.0-20210202160940-bed99a852dfe // indirect
+)
---
+++ rancid/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+
+	"github.com/gen2brain/dlgs"
+)
+
+func main() {
+	file, _, err := dlgs.File("Select file", "", false)
+	if err != nil {
+		dlgs.Error("Error", err.Error())
+		panic(err)
+	}
+
+	numberstring, _, err := dlgs.Entry("Entry", "Enter desired split value... xrodroulh:", "")
+	if err != nil {
+		dlgs.Error("Error", err.Error())
+		panic(err)
+	}
+
+	number, err := strconv.Atoi(numberstring)
+	if err != nil {
+		dlgs.Error("Error", "Xodrouli... mallon ekanes malakia. Prepei na baleis noumeraki")
+		panic(err)
+	}
+
+	if number == 1 {
+		dlgs.Error("Error", "Xodrouli... bale kati megalytero apo 1")
+		panic(err)
+	}
+
+	file2, err := os.Open(file)
+	if err != nil {
+		dlgs.Error("Error", err.Error())
+		panic(err)
+	}
+
+	newfile, err := os.Create("nice.txt")
+	if err != nil {
+		dlgs.Error("Error", err.Error())
+		panic(err)
+	}
+
+	bufioo := bufio.NewScanner(file2)
+	num := 0
+	realcounter := 0
+	buf := new(bytes.Buffer)
+
+	for bufioo.Scan() {
+		num++
+		if scontains(bufioo.Bytes(), '(') {
+			continue
+		} else {
+			realcounter++
+		}
+		if realcounter%number == 1 {
+			buf.Reset()
+		}
+		if realcounter%number == 0 {
+			fmt.Fprintln(buf, bufioo.Text())
+			newfile.Write(buf.Bytes())
+			continue
+		}
+		buf.Write(bufioo.Bytes())
+	}
+	newfile.Write(buf.Bytes())
+
+	stringpath, err := filepath.Abs("nice.txt")
+	if err != nil {
+		dlgs.Error("Error", err.Error())
+		panic(err)
+	}
+	_, err = dlgs.Info("Info", stringpath)
+	if err != nil {
+		dlgs.Error("Error", err.Error())
+		panic(err)
+	}
+
+}
+
+func scontains(s []byte, e rune) bool {
+	z := byte(e)
+	for _, a := range s {
+		if a == z {
+			return true
+		}
+	}
+	return false
+}
