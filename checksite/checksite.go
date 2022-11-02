79b0262e9ffc4c27e3edcd48ee6f30131a8c3b85
Alex Ballas <alex@ballas.org>
2022-11-02 09:38:38 +0200

    Update checksite code

--- checksite/checksite.go
+++ checksite/checksite.go
@@ -14,33 +14,38 @@ func main() {
 start:
 	log.Println("go !")
 	// create chrome instance
-	ctx, cancel := chromedp.NewContext(
-		context.Background(),
-		chromedp.WithLogf(log.Printf),
+
+	opts := append(chromedp.DefaultExecAllocatorOptions[:],
+		chromedp.Flag("headless", false),
 	)
+
+	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
 	defer cancel()
 
+	ctx, cancel2 := chromedp.NewContext(
+		allocCtx,
+		chromedp.WithLogf(log.Printf),
+	)
+	defer cancel2()
+
 	// create a timeout
-	ctx, cancel = context.WithTimeout(ctx, 10*time.Second)
-	defer cancel()
+	ctx, cancel3 := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel3()
+
 	start := time.Now()
 	// navigate to a page, wait for an element, click
-	var res string
 	err := chromedp.Run(ctx,
 		emulation.SetUserAgentOverride("WebScraper 1.0"),
 		chromedp.Navigate(`https://www.eopyykmes.gr/login.xhtml?viewId=/index.xhtml`),
-		// wait for footer element is visible (ie, page is loaded)
-		//chromedp.WaitVisible(`.form-signin`),
-
 		chromedp.WaitVisible(`.container`),
 		chromedp.ActionFunc(func(context.Context) error {
 			log.Printf(">>>>>>>>>>>>>>>>>>>> BOX1 IS VISIBLE")
 			return nil
 		}))
+
 	if err != nil {
 		log.Println(err)
 		goto start
 	}
-	fmt.Printf("h1 contains: '%s'\n", res)
 	fmt.Printf("\nTook: %f secs\n", time.Since(start).Seconds())
 }
--- checksite/go.mod
+++ checksite/go.mod
@@ -3,6 +3,7 @@ module checksite
 go 1.16
 
 require (
-	github.com/chromedp/cdproto v0.0.0-20210227213635-aabfe75e3e09
-	github.com/chromedp/chromedp v0.6.6
+	github.com/chromedp/cdproto v0.0.0-20221029224954-108014bf7279
+	github.com/chromedp/chromedp v0.8.6
+	golang.org/x/sys v0.1.0 // indirect
 )
